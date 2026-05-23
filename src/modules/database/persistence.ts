58e8a19afad7188467edfd033e4829cc3bc48808
Chia1104 <yuyuchia7423@gmail.com>
2026-05-23 21:31:20 +0800

    fix: cleanup

--- src/modules/database/persistence.ts
+++
@@ -1,8 +0,0 @@
-import { createExpoSQLitePersistence } from '@tanstack/expo-db-sqlite-persistence';
-
-import { recordDbExpo } from './client';
-
-export const recordDbPersistence = createExpoSQLitePersistence({
-  // @ts-expect-error - TODO: fix this
-  database: recordDbExpo,
-});
